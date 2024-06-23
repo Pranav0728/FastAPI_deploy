ad82a95d5eae9955ff449df139c31db8738db473
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-06-23 10:04:27 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
